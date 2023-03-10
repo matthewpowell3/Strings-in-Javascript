a9318be2300f649f9316b3e2f4852d580aca5ae8
matthewpowell3 <106132405+matthewpowell3@users.noreply.github.com>
2023-03-10 12:38:56 -0500

    1 file modified

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 // Strings in Javascript
 
-var soup = '"hot" ';
+// var soup = '"hot" ';
 
-document.write(soup);
-document.write(typeof soup);
+// document.write(soup);
+// document.write(typeof soup);
+document.write('Hello there! <br> Welcome to learning strings');
+
+// you can use html elements inside a string
+
+document.write('Hello there! <br> "Welcome" to learning strings');
+
+// to get quotation marks inside a string
